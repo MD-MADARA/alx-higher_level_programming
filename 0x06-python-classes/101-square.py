889a3b526721ed59a5b88f023caf3801b2eae054
MD-MADARA <mouadotaku9421@gmail.com>
2023-09-26 18:20:16 +0100

    __str__

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ A square class """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2\
+                or not isinstance(value[0], int)\
+                or not isinstance(value[1], int)\
+                or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        string = ""
+        if self.__size == 0:
+            pass
+        else:
+            string += ("\n" * self.__position[1])
+            for i in range(self.__size):
+                string += (" " * self.__position[0] + "#" * self.__size)
+                if i < self.__size - 1:
+                    string += "\n"
+        return string
+
+
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
