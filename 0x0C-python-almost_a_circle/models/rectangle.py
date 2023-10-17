2b3592213ba75bce0af8e3f09414f58f5a8704a5
MD-MADARA <mouadotaku9421@gmail.com>
2023-10-18 00:57:04 +0100

    update setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """ Module for Rectangle class """
 from models.base import Base
 
+
 class Rectangle(Base):
     """
     The Rectangle class with attributes for width,
@@ -43,6 +44,10 @@ class Rectangle(Base):
         Args:
             value (int): The new width value.
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -63,6 +68,10 @@ class Rectangle(Base):
         Args:
             value (int): The new height value.
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -83,6 +92,10 @@ class Rectangle(Base):
         Args:
             value (int): The new x-coordinate value.
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -103,4 +116,8 @@ class Rectangle(Base):
         Args:
             value (int): The new y-coordinate value.
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
