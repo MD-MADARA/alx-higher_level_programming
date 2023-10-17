fd7528c531a49ef770c2ccb83d803845ee374c8f
MD-MADARA <mouadotaku9421@gmail.com>
2023-10-18 00:49:10 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from base import Base
 """ Module for Rectangle class """
-
+from base import Base
 
 class Rectangle(Base):
     """
@@ -25,83 +24,3 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
-
-    @property
-    def width(self):
-        """
-        Get the width of the rectangle.
-
-        Returns:
-            int: The width of the rectangle.
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Args:
-            value (int): The new width value.
-        """
-        self.__width = value
-
-    @property
-    def height(self):
-        """
-        Get the height of the rectangle.
-
-        Returns:
-            int: The height of the rectangle.
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-        Set the height of the rectangle.
-
-        Args:
-            value (int): The new height value.
-        """
-        self.__height = value
-
-    @property
-    def x(self):
-        """
-        Get the x-coordinate of the rectangle.
-
-        Returns:
-            int: The x-coordinate.
-        """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """
-        Set the x-coordinate of the rectangle.
-
-        Args:
-            value (int): The new x-coordinate value.
-        """
-        self.__x = value
-
-    @property
-    def y(self):
-        """
-        Get the y-coordinate of the rectangle.
-
-        Returns:
-            int: The y-coordinate.
-        """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """
-        Set the y-coordinate of the rectangle.
-
-        Args:
-            value (int): The new y-coordinate value.
-        """
-        self.__y = value
