bfe6f3d77d2b98cdbe0fa764095dab0938983791
MD-MADARA <mouadotaku9421@gmail.com>
2023-10-19 22:20:57 +0100

    fix2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,6 +44,6 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        dummy = cls(1, 1)
+        dummy = cls(1) if cls.__name__ == "Square" else cls(1, 1)
         dummy.update(**dictionary)
         return dummy
