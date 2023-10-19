5097876a2f718416f149b498021892ef06d2ae15
MD-MADARA <mouadotaku9421@gmail.com>
2023-10-19 22:04:12 +0100

    add create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,9 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(0, 0)
+        dummy.update(**dictionary)
+        return dummy
